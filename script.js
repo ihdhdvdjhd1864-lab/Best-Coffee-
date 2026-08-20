c6751ffd90b459dc67af2356abdefcff546a0304
Karim Mohamed <ihdhdvdjhd1864@gmail.com>
2026-08-20 05:44:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-let observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-      observer.unobserve(entry.target);
-    }
-  });
-}, {
-  threshold: 0.2
-});
+let observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
 
 document.querySelectorAll(".hidden").forEach((el) => {
   observer.observe(el);
 });
 
-
-
-
-
-
-
-
-
 let openbtn = document.querySelector(".openbtn");
 let navul = document.querySelector("nav ul");
+let ovluq = document.querySelector(".ovluq");
 openbtn.addEventListener("click", () => {
   navul.classList.toggle("active");
+  ovluq.classList.toggle("active");
+  if (navul.classList.contains("active")) {
+    openbtn.innerHTML = `<i class="fa fa-times"></i>`;
+  } else {
+    openbtn.innerHTML = `<i class="fa fa-bars"></i>`;
+  }
 });
+ovluq.addEventListener("click", () => {
+  navul.classList.remove("active");
+  ovluq.classList.remove("active");
+  openbtn.innerHTML = `<i class="fa fa-bars"></i>`;
+})
